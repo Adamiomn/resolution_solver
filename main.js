057f4f9640db4dbba2f8ae5f630c1f2de7c1063a
Adamiomn <95017654+Adamiomn@users.noreply.github.com>
2023-04-14 12:11:29 +0200

    approach 3

---
+++ main.js
@@ -0,0 +1,24 @@
+import init, { validate_input, calculate_resolution } from './pkg/resolution_solver.js';
+
+init().then(() => {
+	const inputStringElement = document.getElementById("input-string");
+	const inputValidationElement = document.getElementById("input-validation");
+	const calculateButton = document.getElementById("calculate-btn");
+	const resultElement = document.getElementById("result");
+
+	inputStringElement.addEventListener("input", () => {
+		const input = inputStringElement.value;
+		const isValid = validate_input(input);
+		inputValidationElement.style.visibility = isValid ? "hidden" : "visible";
+	});
+
+	calculateButton.addEventListener("click", () => {
+		const input = inputStringElement.value;
+		if (validate_input(input)) {
+			const resolution = calculate_resolution(input);
+			resultElement.textContent = `The resolution is: ${resolution}`;
+		} else {
+			resultElement.textContent = "Invalid input. Please correct your input and try again.";
+		}
+	});
+});
