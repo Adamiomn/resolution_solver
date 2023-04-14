ccfea73942993383b7d09bd374cbe4bde1f4eef2
Adamiomn <95017654+Adamiomn@users.noreply.github.com>
2023-04-14 12:08:09 +0200

    approach 2

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-import { validate_input, calculate_resolution } from './pkg/resolution_solver.js';
-
 const inputStringElement = document.getElementById("input-string");
 const inputValidationElement = document.getElementById("input-validation");
 const calculateButton = document.getElementById("calculate-btn");
 const resultElement = document.getElementById("result");
 
 inputStringElement.addEventListener("input", () => {
-  const input = inputStringElement.value;
-  const isValid = validate_input(input);
-  inputValidationElement.style.visibility = isValid ? "hidden" : "visible";
+	const input = inputStringElement.value;
+	const isValid = validate_input(input);
+	inputValidationElement.style.visibility = isValid ? "hidden" : "visible";
 });
 
 calculateButton.addEventListener("click", () => {
-  const input = inputStringElement.value;
-  if (validate_input(input)) {
-    const resolution = calculate_resolution(input);
-    resultElement.textContent = `The resolution is: ${resolution}`;
-  } else {
-    resultElement.textContent = "Invalid input. Please correct your input and try again.";
-  }
+	const input = inputStringElement.value;
+	if (validate_input(input)) {
+		const resolution = calculate_resolution(input);
+		resultElement.textContent = `The resolution is: ${resolution}`;
+	} else {
+		resultElement.textContent = "Invalid input. Please correct your input and try again.";
+	}
 });
