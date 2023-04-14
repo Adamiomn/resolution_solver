7934f1ce6e93806ee70760b621de14393886e624
Adamiomn <95017654+Adamiomn@users.noreply.github.com>
2023-04-14 11:38:23 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+import { validate_input, calculate_resolution } from './pkg/your_rust_project.js';
+
+const inputStringElement = document.getElementById("input-string");
+const inputValidationElement = document.getElementById("input-validation");
+const calculateButton = document.getElementById("calculate-btn");
+const resultElement = document.getElementById("result");
+
+inputStringElement.addEventListener("input", () => {
+  const input = inputStringElement.value;
+  const isValid = validate_input(input);
+  inputValidationElement.style.visibility = isValid ? "hidden" : "visible";
+});
+
+calculateButton.addEventListener("click", () => {
+  const input = inputStringElement.value;
+  if (validate_input(input)) {
+    const resolution = calculate_resolution(input);
+    resultElement.textContent = `The resolution is: ${resolution}`;
+  } else {
+    resultElement.textContent = "Invalid input. Please correct your input and try again.";
+  }
+});
